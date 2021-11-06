bae3a125e150eb7347b61580728ae0d445d4832c
Utrel-3007 <90812954+Utrel-3007@users.noreply.github.com>
2021-11-06 15:47:18 +0700

    final update

--- Contacts-main/app/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const createContactModel = require("./contact.model");
-
-const db = {};
-db.mongoose = mongoose;
-db.Contact = createContactModel(mongoose);
-
-module.exports = db;
