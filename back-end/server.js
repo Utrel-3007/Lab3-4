fd9e7e24c754c0caf23bd701037aca810164ac0c
Utrel-3007 <90812954+Utrel-3007@users.noreply.github.com>
2021-11-13 10:20:11 +0700

    edit

--- back-end/server.js
+++ back-end/server.js
@@ -2,50 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const setupAuthRoutes = require("./app/routes/auth.routes");
 const { BadRequestError } = require("./app/helpers/errors");
+const db = require("./app/models");
 
 const app = express();
 
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
-
-app.use(express.urlencoded({
-    extended: true
-}));
-
-setupContactRoutes(app);
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello world!"
-    });
-});
-
-//handle 404 response
-app.use((req, res, next) => {
-    next(new BadRequestError(404, "Resource not found"));
-});
-
-//define error - handling middleware last, after other app.use() and routes calls
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(err.statusCode || 500).json({
-        message: err.message || "Internal Server Error",
-    });
-});
-
-
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running !! ${PORT}`);
-});
-
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-
-db.mongoose.connect(config.db.url)
+db.mongoose
+    .connect(config.db.url)
     .then(() => {
         console.log("Connected to the database!");
     })
@@ -54,6 +22,29 @@ db.mongoose.connect(config.db.url)
         process.exit();
     });
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application" });
+});
+
+setupContactRoutes(app);
+setupAuthRoutes(app);
+
+// handle 404 response
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+// set port, listen for requests
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
